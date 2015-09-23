0e3406321c70ada4999ad80cec9e8f4a220e7331
Ted <ted@lifang.com>
2015-09-23 20:20:52 +0800

    init

---
+++ 3.AndroidCustomView/CustomWidget/app/src/main/java/com/android/ted/customwidget/view/CircleProgressView.java
@@ -0,0 +1,122 @@
+package com.android.ted.customwidget.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Ted on 2015/9/23.
+ */
+public class CircleProgressView extends View {
+
+    private int mMeasureHeigth;
+    private int mMeasureWidth;
+
+    private Paint mCirclePaint;
+    private float mCircleXY;
+    private float mRadius;
+
+    private Paint mArcPaint;
+    private RectF mArcRectF;
+    private float mSweepAngle;
+    private float mSweepValue = 33;
+
+    private Paint mTextPaint;
+    private String mShowText;
+    private float mShowTextSize;
+
+    public CircleProgressView(Context context, AttributeSet attrs,
+                              int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CircleProgressView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircleProgressView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec,
+                             int heightMeasureSpec) {
+        mMeasureWidth = MeasureSpec.getSize(widthMeasureSpec);
+        mMeasureHeigth = MeasureSpec.getSize(heightMeasureSpec);
+        setMeasuredDimension(mMeasureWidth, mMeasureHeigth);
+        initView();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        // 绘制圆
+        canvas.drawCircle(mCircleXY, mCircleXY, mRadius, mCirclePaint);
+        // 绘制弧线
+        canvas.drawArc(mArcRectF, 0, mSweepAngle, false, mArcPaint);
+        // 绘制文字
+        canvas.drawText(mShowText, 0, mShowText.length(),
+                mCircleXY, mCircleXY + (mShowTextSize / 4), mTextPaint);
+    }
+
+    private void initView() {
+        float length = 0;
+        if (mMeasureHeigth >= mMeasureWidth) {
+            length = mMeasureWidth;
+        } else {
+            length = mMeasureHeigth;
+        }
+
+        mCircleXY = length / 2;
+        mRadius = (float) (length * 0.5 / 3);
+        mCirclePaint = new Paint();
+        mCirclePaint.setAntiAlias(true);
+        mCirclePaint.setColor(Color.RED);
+
+        mArcRectF = new RectF(
+                (float) (length * 0.1),
+                (float) (length * 0.1),
+                (float) (length * 0.9),
+                (float) (length * 0.9));
+
+        mSweepAngle = (mSweepValue / 100f) * 360f;
+        mArcPaint = new Paint();
+        mArcPaint.setAntiAlias(true);
+        mArcPaint.setColor(Color.BLUE);
+        mArcPaint.setStrokeWidth((float) (length * 0.1));
+        mArcPaint.setStyle(Paint.Style.STROKE);
+
+        mShowText = setShowText();
+        mShowTextSize = setShowTextSize();
+        mTextPaint = new Paint();
+        mTextPaint.setTextSize(mShowTextSize);
+        mTextPaint.setTextAlign(Paint.Align.CENTER);
+    }
+
+    private float setShowTextSize() {
+        this.invalidate();
+        return 50;
+    }
+
+    private String setShowText() {
+        this.invalidate();
+        return "Android Skill";
+    }
+
+    public void forceInvalidate() {
+        this.invalidate();
+    }
+
+    public void setSweepValue(float sweepValue) {
+        if (sweepValue != 0) {
+            mSweepValue = sweepValue;
+        } else {
+            mSweepValue = 25;
+        }
+        this.invalidate();
+    }
+}
